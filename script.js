246b7d713da8bcb917d6e3b8f609d92decffaf2c
R0binj0 <robin.pruunlep@ametikool.ee>
2023-04-24 08:59:50 +0300

    commit

--- script.js
+++ script.js
@@ -1,55 +1,39 @@
-var aText = new Array(
-"",
-"Welcome to my portfolio! My name is Robin Pruunlep",
-);
-var iSpeed = 30;
-var iIndex = 0; 
-var iArrLength = aText[1].length; 
-var iTextPos = 0; 
-var sContents = '';
-var aText2 = new Array(
-    "",
-    "I am a junior software developer. And I am passionate about creating innovative solutions to complex problems and continuously learning new technologies to improve my skills.",
-    );
-var iArrLength2 = aText2[1].length;  
-     
-function typewriter()
-{
-    sContents =  ' ';
+var string = "Welcome to my portfolio! My name is Robin Pruunlep.";
+var str = string.split("");
+var tt = document.getElementById('typedtext');
 
-    var destination = document.getElementById("typedtext");
-    
-    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
-    if ( iTextPos++ == iArrLength ) {
-        iText.Pos = 0;
-        iIndex++;
-    if ( iIndex != aText.length ) {
-        iArrLength = aText[iIndex].length;
-        setTimeout("typewriter()", 100);
-    }
-    } else {
-    setTimeout("typewriter()", iSpeed);
-    }
-}
-     
-function typewriter2()
-{
-    sContents =  ' ';
+var string2 = "I'm a junior developer who is passionate about the power of programming to transform our world. From simple websites to complex applications, software has the ability to solve real-world problems and create value for its users in ways that were previously unimaginable.";
+var str2 = string2.split("");
+var tt2 = document.getElementById('typedtext2');
 
-    var destination = document.getElementById("typedtext2");
-    
-    destination.innerHTML = sContents + aText2[iIndex].substring(0, iTextPos) + "_";
-    if ( iTextPos++ == iArrLength2 ) {
-        iTextPos = 0;
-        iIndex++;
-    if ( iIndex != aText2.length ) {
-        iArrLength2 = aText2[iIndex].length;
-        setTimeout("typewriter2()", 100);
-    }
-    } else {
-        setTimeout("typewriter2()", iSpeed);
-    }
-}
+function delay(func, delayTime) {
+    setTimeout(func, delayTime);
+};
 
-typewriter();
-typewriter2();
+function typing() {
+
+    str.length > 0 ? tt.innerHTML += str.shift() : clearTimeout(running); 
+    var running = setTimeout(typing, 30);
+
+};
+
+function typing2() {
+
+    str2.length > 0 ? tt2.innerHTML += str2.shift() : clearTimeout(running2); 
+    var running2 = setTimeout(typing2, 25);
+
+};
+
+window.addEventListener('scroll', function() {
+    var scrolled = window.pageYOffset;
+    var first_inside = document.querySelector('.first_inside');
+    var second_inside = document.querySelector('.second_inside');
+    var blur = scrolled * 0.1;
+
+    first_inside.style.filter = "blur(" + blur + "px)";
+    first_inside.style.transform = 'translateY(' + scrolled * 0.5 + 'px)';
+    second_inside.style.transform = 'translateY(' + scrolled * -0.2 + 'px)';
+});
+
+delay(function() {typing()}, 1000);
+delay(function() {typing2()}, 2800);
